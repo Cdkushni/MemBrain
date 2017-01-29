daee13c95b78da7cc6c6e7f44e5f168f89565920
Cdkushni <colinkushniruk@gmail.com>
2017-01-28 18:47:51 -0700

    All that is left for parsing is the coolers, so we took some zeds and food to recuperate.

---
+++ BreakTime.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define StartTime 6
+
+
+
+int main(){
+	// We done bois, my brain hurts. Its time for a break
+	float breakDuration = StartTime * 0.25;
+	float endTime = StartTime + breakDuration;
+	//
+	return endTime;
+}
+
